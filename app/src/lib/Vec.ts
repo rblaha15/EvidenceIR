3ff58278f92a23a692d179ee0d5805c8c94cba47
rblaha15 <rblaha15@student.gymceska.cz>
2022-11-09 23:27:28 +0100

    červená

--- app/src/lib/Vec.ts
+++ app/src/lib/Vec.ts
@@ -1,28 +1,105 @@
 export class Vec {
     nazev: string
     text: string
-    errorFunc: (_: Vec) => string
+    onError: string
     moznosti: string[]
     bool: boolean
     vybrano: string
+    regex: RegExp
+    nutne: boolean
 
     constructor(
         nazev: string = '',
+        onError: string = '',
+        regex: RegExp = /.*/,
+        nutne: boolean = true,
         moznosti: string[] = [],
         bool: boolean = false,
-        errorFunc: (_: Vec) => string = (_) => '',
         text: string = '',
         vybrano: string = '',
     ) {
         this.nazev = nazev
         this.text = text
-        this.errorFunc = errorFunc
+        this.onError = onError
         this.moznosti = moznosti
         this.bool = bool
         this.vybrano = vybrano
+        this.regex = regex
+        this.nutne = nutne
     }
 
-    get error() {
-        return this.errorFunc(this);
+    static Obecna(
+        nazev: string = '',
+        onError: string = '',
+        regex: RegExp = /.*/,
+        nutne: boolean = true,
+        moznosti: string[] = [],
+        bool: boolean = false,
+        text: string = '',
+        vybrano: string = '',
+    ): Vec {
+        return new Vec(
+            nazev,
+            onError,
+            regex,
+            nutne,
+            moznosti,
+            bool,
+            text,
+            vybrano,
+        )
+    }
+    static Vybiratkova(
+        nazev: string = '',
+        moznosti: string[] = [],
+        onError: string = 'Toto pole je povinné',
+        nutne: boolean = true,
+        vybrano: string = '',
+    ): Vec {
+        return new Vec(
+            nazev,
+            onError,
+            /.*/,
+            nutne,
+            moznosti,
+            false,
+            '',
+            vybrano,
+        )
+    }
+    static Pisatkova(
+        nazev: string = '',
+        onError: string = 'Toto pole je povinné',
+        regex: RegExp = /.*/,
+        nutne: boolean = true,
+        text: string = '',
+    ): Vec {
+        return new Vec(
+            nazev,
+            onError,
+            regex,
+            nutne,
+            [],
+            false,
+            text,
+            '',
+        )
+    }
+    static Zaskrtavatkova(
+        nazev: string = '',
+        onError: string = 'Toto pole je povinné',
+        nutne: boolean = true,
+        bool: boolean = false,
+    ): Vec {
+        return new Vec(
+            nazev,
+            onError,
+            /.*/,
+            nutne,
+            [],
+            bool,
+            '',
+            '',
+        )
     }
 }
