47e5909b82329976f658401cf7d0a79aeb1ac8e2
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2022-11-10 16:03:11 +0100

    Delete public directory

--- public/server/chunks/0-b4e93a72.js
+++
@@ -1,8 +0,0 @@
-const index = 0;
-const component = async () => (await import('./layout.svelte-1d4f9fed.js')).default;
-const file = '_app/immutable/components/layout.svelte-aeb9c02d.js';
-const imports = ["_app/immutable/components/layout.svelte-aeb9c02d.js","_app/immutable/chunks/index-ad6009f4.js"];
-const stylesheets = [];
-
-export { component, file, imports, index, stylesheets };
-//# sourceMappingURL=0-b4e93a72.js.map
