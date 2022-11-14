ab1c3734ced93410a81fca2e2279a52d33a6aeb6
rblaha15 <rblaha15@student.gymceska.cz>
2022-11-14 16:43:54 +0100

    TČ a zaškrávátko

--- app/src/lib/Vybiratko.svelte
+++ app/src/lib/Vybiratko.svelte
@@ -4,27 +4,29 @@
 	export let vec: Vec;
 </script>
 
-<label for={vec.nazev}>{vec.nazev}</label>
+{#if vec.zobrazit}
+	<label for={vec.nazev}>{vec.nazev}</label>
 
-<div class="dropdown" id={vec.nazev}>
-	<button type="button" class="btn btn-outline-secondary dropdown-toggle" data-bs-toggle="dropdown">
-		{vec.vybrano != '' ? vec.vybrano : 'Nevybráno'}
-	</button>
-	<ul class="dropdown-menu">
-		{#each vec.moznosti as moznost}
-			<li>
-				<a
-					class="dropdown-item"
-					href="/"
-					on:click={() => {
-						vec.vybrano = moznost;
-					}}>{moznost}</a
-				>
-			</li>
-		{/each}
-	</ul>
-</div>
+	<div class="dropdown" id={vec.nazev}>
+		<button
+			type="button"
+			class="btn btn-outline-secondary dropdown-toggle"
+			data-bs-toggle="dropdown"
+		>
+			{vec.vybrano != '' ? vec.vybrano : 'Nevybráno'}
+		</button>
+		<ul class="dropdown-menu">
+			{#each vec.moznosti as moznost}
+				<li>
+					<a class="dropdown-item" href="/" on:click={() => (vec.vybrano = moznost)}>
+						{moznost}
+					</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
 
-{#if vec.zobrazitError}
-	<p class="text-danger">{vec.onError}</p>
+	{#if vec.zobrazitError}
+		<p class="text-danger">{vec.onError}</p>
+	{/if}
 {/if}
---
+++ app/src/lib/Zaskrtavatko.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Vec } from './Vec';
+
+	export let vec: Vec;
+</script>
+
+{#if vec.zobrazit}
+	<div class="form-check" >
+		<input class="form-check-input" type="checkbox" id={vec.nazev} bind:checked={vec.bool} />
+		<label class="form-check-label" for={vec.nazev}>{vec.nazev}</label>
+	</div>
+
+	{#if vec.zobrazitError}
+		<p class="text-danger">{vec.onError}</p>
+	{/if}
+{/if}
