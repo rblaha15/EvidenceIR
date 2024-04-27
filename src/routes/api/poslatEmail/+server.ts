0af70074643984e2b242230938bf09ccf598f355
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2024-04-27 20:38:17 +0200

    ef

--- src/routes/api/poslatEmail/+server.ts
+++ src/routes/api/poslatEmail/+server.ts
@@ -27,3 +27,12 @@ export const POST: RequestHandler = async ({ request }) => {
 		status: Number(response.slice(0, 3))
 	});
 };
+
+export const GET: RequestHandler = async () => {
+
+	console.log("Hi")
+	
+	return new Response(null, {
+		status: 200
+	});
+}
