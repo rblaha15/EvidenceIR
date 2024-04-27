bd18bc7ff33bce820b04b2fabb681bd1a7daf4dd
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2024-04-27 19:15:34 +0200

    afe

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <svelte:head>
+	<!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
 	<link
 		href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
 		rel="stylesheet"
@@ -13,8 +14,9 @@
 		src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
 		integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
 		crossorigin="anonymous"
-	></script>
+	/>
 </svelte:head>
+
 <title>{dev ? '(dev) ' : ''}Evidence regulátorů IR</title>
 
 <slot />
