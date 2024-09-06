62df347ac7f0d36c0396d54975a138cb9b74eb0b
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2024-09-06 15:56:00 +0200

    Noch mehr Deutsch

--- src/lib/client/pdf/warranty.ts
+++ src/lib/client/pdf/warranty.ts
@@ -8,17 +8,18 @@ export default {
     title: `hpWarranty`,
     fileName: `warrantyFileName`,
     getFormData: async ({ evidence: e }, t) => {
+        console.log(e.montazka)
         const uvedeni = await nazevAdresaFirmy(e.uvedeni.ico, fetch)
         const montazka = await nazevAdresaFirmy(e.montazka.ico, fetch)
         return {
     /*        tcModel */ Text1: t.get(e.tc.model!),
     /*        tcCislo */ Text2: e.tc.cislo,
     /*       montazka */ Text3: `${e.montazka.ico} — ${montazka?.obchodniJmeno ?? ''}`,
-    /* adresaMontazka */ Text4: montazka?.sidlo?.textovaAdresa ?? '',
+    /* adresaMontazka */ Text4: montazka?.sidlo?.textovaAdresa ?? null,
     /* detailMontazka */ Text5: `${e.montazka.email}		${e.montazka.zastupce}`,
     /*        uvedeni */ Text6: `${e.uvedeni.ico} — ${uvedeni?.obchodniJmeno ?? ''} — ${uvedeni?.sidlo?.textovaAdresa ?? ''}`,
     /*  detailUvedeni */ Text7: `${e.uvedeni.email}		${e.uvedeni.zastupce}`,
-    /*       montazka */ Text9: montazka?.obchodniJmeno ?? '',
+    /*       montazka */ Text9: montazka?.obchodniJmeno ?? null,
     /*  clovekUvedeni */ Text11: e.uvedeni.zastupce,
     /*  razitkoProdej */ Text12: '',
     /* razitkoUvedeni */ Text13: '',
