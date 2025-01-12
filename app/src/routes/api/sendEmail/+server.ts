c3666bf07fbbda59c89a13f1cfa01d874e944b2f
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2025-01-12 02:18:52 +0100

    NAROZENINY

--- app/src/routes/api/sendEmail/+server.ts
+++ app/src/routes/api/sendEmail/+server.ts
@@ -38,6 +38,5 @@ export const OPTIONS = async () => new Response(null, {
     headers: {
         'Access-Control-Allow-Origin': 'https://janda-ma-narozeniny.web.app',
         'Access-Control-Allow-Methods': 'POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
     },
 });
