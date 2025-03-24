8834e5258a53798c434cc7023bf46164cfe914ba
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2025-03-24 22:50:59 +0100

    gcjvhkb

--- app/src/lib/emails/MailSDaty.svelte
+++ app/src/lib/emails/MailSDaty.svelte
@@ -30,35 +30,36 @@
 		host
 	}: Props = $props();
 
+    const d = $derived({ d: data })
+
 	let list = $derived((Object.values(data) as Data[keyof Data][]).flatMap(
-		(obj) => Object.values(obj) as Widget<Data, unknown>[]
+		(obj) => Object.values(obj) as Widget<{ d: Data }, unknown>[]
 	));
 
-    const irid = extractIRIDFromParts(data.ir.typ.value.first, data.ir.cislo.value
-)
+    const irid = extractIRIDFromParts(data.ir.typ.value.first!, data.ir.cislo.value)
 </script>
 
 <p>Odkaz na podrobnosti evidence: <a href={host + `/detail/${irid}`}>{host + `/detail/${irid}`}</a></p>
 
 {#each list as vec}
-	{#if vec instanceof TitleWidget && vec.showTextValue(data)}
-		<h2>{t.get(vec.label(data))}</h2>
-	{:else if vec instanceof TextWidget && vec.showTextValue(data)}
-		<p>{t.get(vec.label(data))}</p>
-	{:else if vec instanceof InputWidget && vec.showTextValue(data)}
-		<p><b>{t.get(vec.label(data))}</b>: {vec.value}</p>
-	{:else if vec instanceof DoubleChooserWidget && vec.showTextValue(data)}
-		<p><b>{t.get(vec.label(data))}</b>: {t.getN(vec.value.first) ?? ''} {t.getN(vec.value.second) ?? ''}</p>
-	{:else if vec instanceof ChooserWidget && vec.showTextValue(data)}
-		<p><b>{t.get(vec.label(data))}</b>: {t.getN(vec.value) ?? ''}</p>
-	{:else if vec instanceof RadioWidget && vec.showTextValue(data)}
-		<p><b>{t.get(vec.label(data))}</b>: {t.getN(vec.value) ?? ''}</p>
-	{:else if vec instanceof SwitchWidget && vec.showTextValue(data)}
-		<p><b>{t.get(vec.label(data))}</b>: {t.get(vec.value ? vec.options[1] : vec.options[0])}</p>
-	{:else if vec instanceof MultiCheckboxWidget && vec.showTextValue(data)}
-		<p><b>{t.get(vec.label(data))}</b>: {vec.value.map(s => t.get(s)).join(', ')}</p>
-	{:else if vec instanceof CheckboxWidget && vec.showTextValue(data)}
-		<p><b>{t.get(vec.label(data))}</b>: {vec.value ? t.yes : t.no}</p>
+	{#if vec instanceof TitleWidget && vec.showTextValue(d)}
+		<h2>{t.get(vec.label(d))}</h2>
+	{:else if vec instanceof TextWidget && vec.showTextValue(d)}
+		<p>{t.get(vec.label(d))}</p>
+	{:else if vec instanceof InputWidget && vec.showTextValue(d)}
+		<p><b>{t.get(vec.label(d))}</b>: {vec.value}</p>
+	{:else if vec instanceof DoubleChooserWidget && vec.showTextValue(d)}
+		<p><b>{t.get(vec.label(d))}</b>: {t.getN(vec.value.first) ?? ''} {t.getN(vec.value.second) ?? ''}</p>
+	{:else if vec instanceof ChooserWidget && vec.showTextValue(d)}
+		<p><b>{t.get(vec.label(d))}</b>: {t.getN(vec.value) ?? ''}</p>
+	{:else if vec instanceof RadioWidget && vec.showTextValue(d)}
+		<p><b>{t.get(vec.label(d))}</b>: {t.getN(vec.value) ?? ''}</p>
+	{:else if vec instanceof SwitchWidget && vec.showTextValue(d)}
+		<p><b>{t.get(vec.label(d))}</b>: {t.get(vec.value ? vec.options[1] : vec.options[0])}</p>
+	{:else if vec instanceof MultiCheckboxWidget && vec.showTextValue(d)}
+		<p><b>{t.get(vec.label(d))}</b>: {vec.value.map(s => t.get(s)).join(', ')}</p>
+	{:else if vec instanceof CheckboxWidget && vec.showTextValue(d)}
+		<p><b>{t.get(vec.label(d))}</b>: {vec.value ? t.yes : t.no}</p>
 	{/if}
 {/each}
 <p><b>Zaevidoval</b>: {user.email}</p>
