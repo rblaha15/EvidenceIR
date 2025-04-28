df3acab29863da54929084924386ef9092531352
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2025-04-28 17:58:19 +0200

    Settings improvements

--- app/src/lib/components/Settings.svelte
+++ app/src/lib/components/Settings.svelte
@@ -1,30 +1,41 @@
 <script lang="ts">
-    import type { Translations } from '$lib/translations';
-    import ThemeSelector from "$lib/components/ThemeSelector.svelte";
-    import LanguageSelector from "$lib/components/LanguageSelector.svelte";
-    import { removeAllPhotos } from "$lib/components/widgets/PhotoSelector.svelte";
-    import { browser, dev, version } from "$app/environment";
+	import type { Translations } from '$lib/translations';
+	import ThemeSelector from '$lib/components/ThemeSelector.svelte';
+	import LanguageSelector from '$lib/components/LanguageSelector.svelte';
+	import { removeAllPhotos } from '$lib/components/widgets/PhotoSelector.svelte';
+	import { browser, dev, version } from '$app/environment';
 
-    interface Props {
-        t: Translations;
-    }
+	interface Props {
+		t: Translations;
+	}
 
-    const { t }: Props = $props();
+	const { t }: Props = $props();
 
-    const clearAll = async () => {
-        localStorage.clear()
-        await removeAllPhotos()
-        window.location.reload()
-    }
+	const clearAll = async () => {
+		localStorage.clear();
+		await removeAllPhotos();
+		window.location.reload();
+	};
 </script>
 
-<p class="d-flex align-items-center"><span class="me-1">{t.appTheme}:</span>
-    <ThemeSelector {t} />
-</p>
-<p class="d-flex align-items-center"><span class="me-1">{t.language}:</span>
-    <LanguageSelector />
-</p>
+<div class="alert alert-primary">
+    <h4 class="alert-heading">Uživatelská nastavení</h4>
+    <div class="d-flex align-items-center"><span class="me-1">{t.appTheme}:</span>
+        <ThemeSelector {t} />
+    </div>
+    <div class="d-flex align-items-center"><span class="me-1">{t.language}:</span>
+        <LanguageSelector />
+    </div>
+</div>
 
-<p>Verze aplikace: {appVersion} ({dev ? 'DEV' : browser ? 'BROWSER' : 'UNKNOWN'})</p>
-<p>Commit: {version}</p>
-<button class="btn btn-danger" onclick={clearAll}>Vymazat všechny uložené formuláře</button>
+<div class="alert alert-info">
+    <h4 class="alert-heading">Informace o aplikaci</h4>
+    <p class="mb-0">Verze aplikace: {appVersion} ({version.slice(0, 7)}) ({dev ? 'DEV' : browser ? 'BROWSER' : 'UNKNOWN'})</p>
+</div>
+
+<div class="alert alert-warning">
+    <h4 class="alert-heading">Vyčistit data z prohlížeče</h4>
+    <p>Data stránky v prohlížeči obsahují vaše uživatelská nastavení, stránky uložené do offline režimu a všechny rozepsané formuláře. Neodstraní se žádná data,
+        která již byla uložena a odeslána na server. Nebudete odhlášeni ze svého účtu.</p>
+    <button class="btn btn-warning btn-sm" onclick={clearAll}>Vyčistit data z prohlížeče</button>
+</div>
