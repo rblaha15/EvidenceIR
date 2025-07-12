920a4948e227b716e4529917bc01366e222cc6da
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2025-07-12 12:57:58 +0200

    #77 XML evidence se odešle při každé nové registraci

---
+++ src/lib/emails/MailXML.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { type FormIN } from '$lib/forms/IN/formIN';
+	import type { Translations } from '$lib/translations';
+	import { irName, irLabel, extractIRIDFromParts } from '$lib/helpers/ir';
+	import type { Raw } from '$lib/forms/Form';
+	import { detailIrUrl } from '$lib/helpers/runes.svelte';
+
+	interface Props {
+		e: Raw<FormIN>;
+        origin: string;
+	}
+
+	let {
+		e,
+        origin,
+	}: Props = $props();
+
+    const irid = extractIRIDFromParts(e.ir.typ.first!, e.ir.cislo);
+</script>
+
+<p>Odkaz na podrobnosti evidence: <a href={origin + detailIrUrl(irid)}>{origin + detailIrUrl(irid)}</a></p>
