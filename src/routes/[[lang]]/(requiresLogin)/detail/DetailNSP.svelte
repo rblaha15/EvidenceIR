1b9f5e8f966edc2d01c5c47281c29c5f736fdfe9
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2025-07-29 21:19:11 +0200

    #124 Při vytváření kopie NSP se budou kopírovat pouze informace o instalaci

--- src/routes/[[lang]]/(requiresLogin)/detail/DetailNSP.svelte
+++ src/routes/[[lang]]/(requiresLogin)/detail/DetailNSP.svelte
@@ -2,17 +2,21 @@
     import { detailIrUrl, detailSpUrl, relUrl, spidUrl } from '$lib/helpers/runes.svelte';
     import PDFLink from './PDFLink.svelte';
     import { storable } from '$lib/helpers/stores';
-    import NSP from '$lib/forms/IN/infoIN';
+    import NSP from '$lib/forms/NSP/infoNSP';
     import { isUserAdmin } from '$lib/client/auth';
     import Widget from '$lib/components/Widget.svelte';
     import db from '$lib/client/data';
     import { goto } from '$app/navigation';
     import type { LanguageCode } from '$lib/languages';
     import { p, type Translations } from '$lib/translations';
-    import type { FormNSP } from '$lib/forms/NSP/formNSP';
+    import { defaultNSP, type FormNSP } from '$lib/forms/NSP/formNSP';
     import type { Raw } from '$lib/forms/Form';
     import type { IRID, SPID } from '$lib/helpers/ir';
     import { InputWidget } from '$lib/forms/Widget.svelte';
+    import defaultSP from '$lib/forms/SP/defaultSP';
+    import type { FormSP } from '$lib/forms/SP/formSP.svelte';
+    import { dataToRawData } from '$lib/forms/Form.js';
+    import type { EventHandler } from 'svelte/elements';
 
     const {
         t, sp, spid, lang,
@@ -20,19 +24,24 @@
         t: Translations, sp: Raw<FormNSP>, spid: SPID, lang: LanguageCode,
     } = $props()
 
+    const protocolGroups: (keyof Raw<FormSP>)[] = defaultSP().keys()
+
     const newIRID = new InputWidget({
         label: p('IRID (z URL adresy)'),
     });
     const transfer = async () => {
         if (!sp) return;
-        await db.addServiceProtocol(newIRID.value as IRID, {
-            zasah: sp.zasah, fakturace: sp.fakturace, ukony: sp.ukony, nahradniDil1: sp.nahradniDil1,
-            nahradniDil2: sp.nahradniDil2, nahradniDil3: sp.nahradniDil3, nahradniDil4: sp.nahradniDil4,
-            nahradniDil5: sp.nahradniDil5, nahradniDil6: sp.nahradniDil6, nahradniDil7: sp.nahradniDil7,
-            nahradniDil8: sp.nahradniDil8, nahradniDily: sp.nahradniDily,
-        });
+        await db.addServiceProtocol(newIRID.value as IRID, sp.pick(...protocolGroups) as Raw<FormSP>);
         await goto(detailIrUrl(newIRID.value as IRID), { replaceState: true });
     };
+
+    const createCopy = () => {
+        const newSP = {
+            ...dataToRawData(defaultNSP()),
+            ...sp.omit(...protocolGroups),
+        }
+        storable<typeof sp>(NSP.storeName).set(newSP);
+    }
 </script>
 
 <div class="d-flex flex-column gap-1 align-items-sm-start">
@@ -48,9 +57,7 @@
 
         <PDFLink {lang} {t} link="NSP" hideLanguageSelector={true} data={sp} {spid} />
 
-        <a class="btn btn-warning" href={relUrl('/NSP')} onclick={() => {
-            storable<typeof sp>(NSP.storeName).set(sp)
-        }}>Vytvořit kopii protokolu</a>
+        <a class="btn btn-warning" href={relUrl('/NSP')} onclick={createCopy}>Kopírovat inpormace o instalaci do nového protokolu</a>
     </div>
 
     {#if $isUserAdmin}
