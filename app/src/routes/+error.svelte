c78d711bc2a5ff8664601f9f97bb279e286b936c
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2025-08-23 10:35:15 +0200

    #144 Změněn vzhled errror stránek

---
+++ app/src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { page } from '$app/state';
+    import { setTitle } from '$lib/helpers/globals';
+    import type { LayoutData } from './$types';
+
+    const { data }: { data: LayoutData } = $props();
+    const t = $derived(data.translations);
+
+    $effect(() => setTitle(t.auth.somethingWentWrong, true));
+</script>
+
+<h3>
+    {#if page.status === 404}
+        {t.auth.siteDoesNotExist}
+    {:else if page.status === 401}
+        {t.auth.noAccess}
+    {:else}
+        Error {page.status}: {page.error?.message}
+    {/if}
+</h3>
