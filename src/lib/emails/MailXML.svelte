bd8bb7bda8e8a9fb9d5ad18e93fcb065e588d760
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2025-09-02 13:47:48 +0200

    Přidán evidující uživatel do emailů

--- src/lib/emails/MailXML.svelte
+++ src/lib/emails/MailXML.svelte
@@ -4,18 +4,23 @@
 	import { irName, irLabel, extractIRIDFromParts } from '$lib/helpers/ir';
 	import type { Raw } from '$lib/forms/Form';
 	import { detailIrUrl } from '$lib/helpers/runes.svelte';
+    import type { User } from 'firebase/auth';
 
 	interface Props {
 		e: Raw<FormIN>;
         origin: string;
+        user: User;
 	}
 
-	let {
+	const {
 		e,
         origin,
+        user,
 	}: Props = $props();
 
     const irid = extractIRIDFromParts(e.ir.typ.first!, e.ir.cislo);
 </script>
 
 <p>Odkaz na podrobnosti evidence: <a href={origin + detailIrUrl(irid)}>{origin + detailIrUrl(irid)}</a></p>
+
+<p><b>Zaevidoval</b>: {user.displayName || user.email}</p>
