dda858d2d451767929acfc7c085e4f0b0eb9c140
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2025-11-03 18:31:41 +0100

    Nastavení tlaků v UPS

--- src/lib/pdf/generators/pdfZLS.ts
+++ src/lib/pdf/generators/pdfZLS.ts
@@ -2,7 +2,7 @@ import ares from '$lib/helpers/ares';
 import type { GetPdfData } from '$lib/pdf/pdf';
 import { today } from '$lib/helpers/date';
 
-const pdfZLS: GetPdfData<'ZLS'> = async ({ data: { evidence: e }, t }) => {
+const pdfZLS: GetPdfData<'ZLS'> = async ({ data: { evidence: e, uvedeniSOL: u }, t }) => {
     const uvedeni = await ares.getNameAndAddress(e.uvedeni.ico, fetch);
     const montazka = await ares.getNameAndAddress(e.montazka.ico, fetch);
     return {
@@ -13,8 +13,8 @@ const pdfZLS: GetPdfData<'ZLS'> = async ({ data: { evidence: e }, t }) => {
         Text5: `${e.montazka.email ?? ''} — ${e.montazka.telefon ?? ''}\t\t${e.montazka.zastupce ?? ''}`,
         Text6: `${e.uvedeni.ico} — ${uvedeni?.obchodniJmeno ?? ''} — ${uvedeni?.sidlo?.textovaAdresa ?? ''}`,
         Text7: `${e.uvedeni.email ?? '' } — ${e.uvedeni.telefon ?? '' }\t\t${e.uvedeni.zastupce ?? ''}`,
-        Text8: '', // TODO
-        Text10: '', // TODO
+        Text8: u?.sol.tlakKapaliny || null,
+        Text10: u?.sol.tlakEnSol || null,
         Text9: montazka?.obchodniJmeno ?? null,
         Text11: e.uvedeni.zastupce,
         Text12: null,
