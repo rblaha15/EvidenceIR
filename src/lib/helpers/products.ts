322373f11009b2556f3e5c665825d26a0a83d5c6
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2025-12-01 17:00:08 +0100

    EcoHeat

--- src/lib/helpers/products.ts
+++ src/lib/helpers/products.ts
@@ -42,7 +42,7 @@ export const heatPumps = {
         'EcoHeat 406',
         'EcoHeat 408',
         'EcoHeat 410',
-        'EcoPart 412',
+        'EcoHeat 412',
         'EcoHeat 306',
         'EcoHeat 308',
         'EcoHeat 310',
