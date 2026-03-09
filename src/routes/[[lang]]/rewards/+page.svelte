24a75c7ac0eb2814da39748dc5571385dfcdf62b
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2026-03-09 20:17:01 +0100

    Error kódy u VP

--- src/routes/[[lang]]/rewards/+page.svelte
+++ src/routes/[[lang]]/rewards/+page.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
     import type { PageProps } from './$types';
     import PdfPreview from '$lib/components/pdf/PdfPreview.svelte';
+    import Icon from '$lib/components/Icon.svelte';
+    import { downloadFile } from '$lib/helpers/files';
 
     const {
         data,
     }: PageProps = $props();
 
     const t = $derived(data.translations.pdf);
+
+    const download = async () => downloadFile(data.url, data.fileName);
 </script>
 
-<PdfPreview {t} url={data.url} />
+<PdfPreview {t} url={data.url}>
+    <button class="btn btn-primary" onclick={download}>
+        <Icon icon="file_download" />
+        {t.downloadFile}
+    </button>
+</PdfPreview>
