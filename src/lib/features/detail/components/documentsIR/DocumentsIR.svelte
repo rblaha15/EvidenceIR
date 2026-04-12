18e367188b2728d3aa929d720ab50a9a2585a595
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2026-04-12 13:18:05 +0200

    Allow button

--- src/lib/features/detail/components/documentsIR/DocumentsIR.svelte
+++ src/lib/features/detail/components/documentsIR/DocumentsIR.svelte
@@ -2,24 +2,21 @@
     import type { Translations } from '$lib/translations';
     import { type ExistingIR } from '$lib/data';
     import { type IRID } from '$lib/helpers/ir';
-    import { currentUser, isUserAdmin, isUserRegulusOrAdmin } from '$lib/client/auth';
+    import { isUserAdmin, isUserRegulusOrAdmin } from '$lib/client/auth';
     import PDFLink from './PDFLink.svelte';
     import type { LanguageCode } from '$lib/languageCodes';
     import RefsiteModals from '$lib/features/detail/components/documentsIR/RefsiteModals.svelte';
     import { createDocumentLinks } from '$lib/features/detail/domain/documentsIR/createDocumentLinks';
     import { pdfInfo } from '$lib/pdf/pdf';
-    import { usersList } from '$lib/client/realtime';
+    import { allowUPT } from '$lib/client/realtime';
 
     const { t, ir, irid, lang }: {
         t: Translations, ir: ExistingIR, irid: IRID, lang: LanguageCode
     } = $props();
     const td = $derived(t.detail);
 
-    const user = $derived($usersList.find(u => u.email == $currentUser?.email))
-    const allowUPT = $derived(user?.allowUPT ?? false)
-
     const links = $derived(
-        createDocumentLinks(ir, t, { isAdmin: $isUserAdmin, isRegulusOrAdmin: $isUserRegulusOrAdmin, allowUPT })
+        createDocumentLinks(ir, t, { isAdmin: $isUserAdmin, isRegulusOrAdmin: $isUserRegulusOrAdmin, allowUPT: $allowUPT })
     )
 </script>
 
