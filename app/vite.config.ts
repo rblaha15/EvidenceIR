c73cccd55c3f7e48a5f855f02052e4c05f887c50
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2026-04-15 16:41:17 +0200

    Base setup

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,5 +1,17 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import pkg from './package.json';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+    plugins: [tailwindcss(), sveltekit()],
+    server: {
+        port: 5005,
+        strictPort: true,
+    },
+    define: {
+        appVersion: JSON.stringify(pkg.version),
+    },
+    envDir: '../',
+    envPrefix: 'PUBLIC_',
+});
