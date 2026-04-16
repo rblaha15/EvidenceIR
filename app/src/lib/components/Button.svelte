63662d87bf650943e84181734de0a5d73bc76a15
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2026-04-16 18:54:40 +0200

    Button

--- app/src/lib/components/Button.svelte
+++ app/src/lib/components/Button.svelte
@@ -1,32 +1,30 @@
 <script lang="ts" module>
-    import type { Color } from '$lib/forms/Widget';
     import type { ClassValue, EventHandler, HTMLAttributeAnchorTarget } from 'svelte/elements';
-    import type { Snippet } from 'svelte';
-
-    type AtLeastOne<T> = {
-        [K in keyof T]: Pick<T, K> & Partial<Omit<T, K>>
-    }[keyof T]
+    import type { Component, Snippet } from 'svelte';
+    import type { LucideProps } from "@lucide/svelte";
+    import type { ButtonSize, ButtonVariant } from "$lib/components/ui/button";
+    import type { AtLeastOne } from "$lib/utils";
 
     type IconSettings = {
-        icon?: string,
+        icon?: Component<LucideProps>,
         iconClass?: ClassValue,
     } | {
-        icons: string[],
+        icons: Component<LucideProps>[],
     };
     type AnchorSettings = {
         href: string,
         target?: HTMLAttributeAnchorTarget,
-        dismissModal?: boolean,
-        element?: HTMLAnchorElement,
+        // dismissModal?: boolean,
+        ref?: HTMLAnchorElement | null,
     };
     type ButtonElementSettings = {
-        element?: HTMLButtonElement,
+        ref?: HTMLButtonElement | null,
     } & AtLeastOne<{
-        modalID: string,
-        offcanvasID: string,
+        // modalID: string,
+        // offcanvasID: string,
         onclick: EventHandler<MouseEvent, HTMLButtonElement>,
-        dismissModal: boolean,
-        toggleDropdown: boolean,
+        // dismissModal: boolean,
+        // toggleDropdown: boolean,
     }>;
     type ActionSettings = AnchorSettings | ButtonElementSettings;
     type LabelSettings = AtLeastOne<{
@@ -37,76 +35,42 @@
     }>
     type StyleSettings = {
         disabled?: boolean,
-        color?: Color,
-        link?: boolean,
-        square?: boolean,
-        outline?: boolean,
+        variant?: ButtonVariant;
+        size?: ButtonSize;
         class?: ClassValue,
     } & LabelSettings & IconSettings;
     export type ButtonSettings = StyleSettings & ActionSettings
 </script>
 
 <script lang="ts">
-    import Icon from '$lib/components/Icon.svelte';
+    import { Button } from "$lib/components/ui/button";
 
     let {
-        element = $bindable(),
+        ref = $bindable(null),
         ...settings
     }: ButtonSettings = $props();
 
-    const outline = $derived(settings.outline ? 'outline-' : '');
-    const color = $derived(settings.link ? ['btn', 'btn-link', 'link-offset-1']
-        : settings.color ? ['btn', `btn-${outline}${settings.color}`] : '');
-    const square = $derived({ square: settings.square });
-    const klass = $derived([color, 'text-nowrap', square, settings.class]);
     const icons = $derived('icons' in settings ? settings.icons : settings.icon ? [settings.icon] : []);
     const children = $derived(settings.children || settings.content);
 </script>
 
-{#snippet content()}
+<Button
+    disabled={settings.disabled}
+    href={'href' in settings ? settings.href : undefined}
+    target={'href' in settings ? settings.target : undefined}
+    tabindex={0}
+    aria-label={settings.label}
+    variant={settings.variant}
+    size={settings.size}
+    class={settings.class}
+    bind:ref
+>
     {#if settings.text}
         {settings.text}
     {:else}
-        {#each icons as icon}
-            <Icon {icon} class={'iconClass' in settings ? settings.iconClass : undefined} />
+        {#each icons as Icon}
+            <Icon class={'iconClass' in settings ? settings.iconClass : undefined} />
         {/each}
     {/if}
     {@render children?.()}
-{/snippet}
-
-{#if 'href' in settings}
-    <a
-        class={[klass, { disabled: settings.disabled }]}
-        href={settings.href}
-        target={settings.target}
-        tabindex="0"
-        data-bs-dismiss={settings.dismissModal ? "modal" : undefined}
-        bind:this={element}
-        aria-label={settings.label}
-    >{@render content()}</a>
-{:else}
-    <button
-        type="button"
-        class={klass}
-        disabled={settings.disabled}
-        data-bs-toggle={settings.modalID ? 'modal' : settings.offcanvasID ? 'offcanvas'
-            : settings.toggleDropdown ? 'dropdown' : undefined}
-        data-bs-target={settings.modalID || settings.offcanvasID
-            ? `#${settings.modalID || settings.offcanvasID}` : undefined}
-        aria-controls={settings.modalID || settings.offcanvasID}
-        onclick={e => settings.onclick?.(e)}
-        data-bs-dismiss={settings.dismissModal ? "modal" : undefined}
-        bind:this={element}
-        aria-label={settings.label}
-    >{@render content()}</button>
-{/if}
-
-<!--suppress CssUnusedSymbol -->
-<style>
-    .square {
-        --bs-btn-padding-x: var(--bs-btn-padding-y)
-    }
-    .btn-link {
-        --bs-btn-padding-x: 0;
-    }
-</style>
+</Button>
