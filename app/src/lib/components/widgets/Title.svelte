61a6758e1640ea275a1b5eccba0c48c902dc381e
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2026-04-22 21:13:39 +0200

    TOC

--- app/src/lib/components/widgets/Title.svelte
+++ app/src/lib/components/widgets/Title.svelte
@@ -14,19 +14,19 @@
 
 {#await widget.text(t, context) then text}
     {#if text}
-        {@const attrs = { id: parseTitleId(text), class: [widget.class(context), 'm-0'] }}
+        {@const props = { id: parseTitleId(text), class: widget.class(context) }}
         {#if widget.level === 1}
-            <h1 {...attrs}>{text}</h1>
+            <h1 {...props}>{text}</h1>
         {:else if widget.level === 2}
-            <h2 {...attrs}>{text}</h2>
+            <h2 {...props}>{text}</h2>
         {:else if widget.level === 3}
-            <h3 {...attrs}>{text}</h3>
+            <h3 {...props}>{text}</h3>
         {:else if widget.level === 4}
-            <h4 {...attrs}>{text}</h4>
+            <h4 {...props}>{text}</h4>
         {:else if widget.level === 5}
-            <h5 {...attrs}>{text}</h5>
+            <h5 {...props}>{text}</h5>
         {:else if widget.level === 6}
-            <h6 {...attrs}>{text}</h6>
+            <h6 {...props}>{text}</h6>
         {/if}
     {/if}
 {/await}
