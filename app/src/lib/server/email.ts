63ebab6616e207bcca370cd970e001eee3c2177d
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2026-08-20 15:13:52 +0200

    email env

--- app/src/lib/server/email.ts
+++ app/src/lib/server/email.ts
@@ -1,21 +1,15 @@
 import { env } from '$env/dynamic/private';
+import nodemailer from 'nodemailer';
 import type { EmailMessage } from '$lib/client/email';
-import nodemailer, { type Transporter } from 'nodemailer';
 
-let transporter: Transporter;
+const transporter = nodemailer.createTransport({
+    host: env.EMAIL_SMTP_HOST,
+    port: env.EMAIL_SMTP_PORT.toNumber(),
+    secure: true,
+    auth: {
+        user: env.EMAIL_USERNAME,
+        pass: env.EMAIL_PASSWORD,
+    },
+});
 
-const getTransporter = () => {
-    transporter ||= nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: env.EMAIL_USERNAME_G.also(console.log),
-            pass: env.EMAIL_PASSWORD_G.also(console.log),
-        },
-    });
-
-    return transporter;
-}
-
-export const sendEmail = (message: EmailMessage) => getTransporter().sendMail(message).thenAlso(console.log);
+export const sendEmail = (message: EmailMessage) => transporter.sendMail(message);
