274b576bef75b862b504f26d74e4e5bc97a5dabe
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2026-08-20 16:45:46 +0200

    tls

--- app/src/lib/server/email.ts
+++ app/src/lib/server/email.ts
@@ -6,6 +6,7 @@ const transporter = nodemailer.createTransport({
     host: env.EMAIL_SMTP_HOST,
     port: Number(env.EMAIL_SMTP_PORT),
     secure: env.EMAIL_SMTP_SECURE != 'false',
+    requireTLS: env.EMAIL_SMTP_SECURE != 'false',
     logger: true,
     debug: true,
     auth: {
