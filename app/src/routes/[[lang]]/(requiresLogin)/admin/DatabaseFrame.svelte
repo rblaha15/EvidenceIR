784f7c3f5838c3c58f99310655588307bbb15eaf
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2026-08-21 13:09:21 +0200

    db view adjustments

--- app/src/routes/[[lang]]/(requiresLogin)/admin/DatabaseFrame.svelte
+++ app/src/routes/[[lang]]/(requiresLogin)/admin/DatabaseFrame.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
     import { page } from '$app/state';
+    import { Button } from '$lib/components/ui/button';
     import type { PageData } from './$types';
 
     const { dbLink } = page.data as PageData;
 </script>
 
-<iframe src={dbLink} title="Databáze"></iframe>
+<Button href={dbLink} target="_blank">Otevřít na nové kartě</Button>
+
+<iframe src={dbLink} title="Databáze" height="10000"></iframe>
