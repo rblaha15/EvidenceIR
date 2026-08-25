798c87ba0b17115312144c064393e44ff585e486
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2026-08-25 13:13:31 +0200

    removed fb hooks

--- app/src/hooks.client.ts
+++ app/src/hooks.client.ts
@@ -1,24 +1,8 @@
-import { getApps, initializeApp, type FirebaseOptions } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-
-const firebaseConfig: FirebaseOptions = {
-	apiKey: 'AIzaSyCKu8Z4wx55DfrZdYtKvrqvwZ2Y6nQvx24',
-	authDomain: 'evidence-ir.firebaseapp.com',
-	projectId: 'evidence-ir',
-	storageBucket: 'evidence-ir.appspot.com',
-	messagingSenderId: '1021340777991',
-	appId: '1:1021340777991:web:d44750968c2d8dbc8834a2',
-	measurementId: 'G-8KZH7Q0ZLC',
-	databaseURL: 'https://evidence-ir-default-rtdb.europe-west1.firebasedatabase.app/',
-};
-
-export const app = getApps().length > 0 ? getApps()[0] : initializeApp(firebaseConfig)
-
-export const auth = getAuth(app);
-
 declare global {
 	/**
 	 * Delared in vite.config.ts
 	 */
 	const appVersion: string;
 }
+
+export {};
