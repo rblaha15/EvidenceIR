d1ae98b50df4e7c837fda9c51451933b7791da66
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2026-08-26 17:07:14 +0200

    hopefully UPT LP fix (2)

--- src/lib/server/loyaltyProgram.ts
+++ src/lib/server/loyaltyProgram.ts
@@ -51,7 +51,7 @@ export const processLoyaltyReward = async (
         const { assembly, commissioning, pumpCount, granted, commissionDate } = d;
         if (granted) return;
         const now = new Date();
-        if (commissionDate.valueOf() + days(180) < now.valueOf()) return;
+        if (commissionDate && commissionDate.valueOf() + days(180) < now.valueOf()) return;
         if (assembly) {
             const current = await getLoyaltyProgramData(assembly);
             if (current.history.some(t => t.type == 'heatPumpAssembly' && t.irid == data.irid)) return;
@@ -89,12 +89,12 @@ const getCompanyUser = async (email: string) => {
 
 const getCompaniesCascadeGrantedAndCommission = async (irid: IRID) => {
     const ir = await getIR(irid);
-    return ir && ir.UP.dateTC ? {
+    return ir ? {
         assembly: await getCompanyUser(ir.IN.montazka.email),
         commissioning: await getCompanyUser(ir.IN.uvedeni.email),
         pumpCount: cascadePumps(ir.IN).length,
         granted: ir.meta.flags?.grantedCommission ?? false,
-        commissionDate: new Date(ir.UP.dateTC),
+        commissionDate: ir.UP.dateTC && new Date(ir.UP.dateTC),
     } : null;
 };
 
